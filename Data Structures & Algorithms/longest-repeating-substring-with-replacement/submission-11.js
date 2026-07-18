ab3e34d3bfa2e25eb78148d503a060c7011dff2e
Ali Ozzaim <125793435+Aliozzaim@users.noreply.github.com>
2026-07-18 22:19:42 +0200

    Add: longest-repeating-substring-with-replacement - submission-11

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-11.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        let l = 0;
+        let maxFreq = 0;
+        let freq = new Map();
+        let result = 0;
+        // [X Y ] Y  X
+        for (let r = 0; r < s.length; r++) {
+            let ch = s[r]
+            freq.set(ch, (freq.get(ch) || 0) + 1);
+            maxFreq = Math.max(maxFreq, freq.get(ch));
+            while ((r - l + 1) - maxFreq > k) {
+                const leftChar = s[l];
+                freq.set(leftChar, freq.get(leftChar) - 1);
+                l++;
+            }
+            result = Math.max(result, r - l + 1);
+        }
+    return result;
+
+    }
+}
